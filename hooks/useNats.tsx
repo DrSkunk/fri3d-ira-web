18e94fde2164f2c8f1224fed4def4829cd925984
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2024-05-02 21:04:35 +0200

    nats hook

---
+++ hooks/useNats.tsx
@@ -0,0 +1,41 @@
+import { connect, Msg, NatsConnection, NatsError } from "nats.ws";
+import { useState, useEffect } from "react";
+
+export const natsClientPromise = connect({ servers: "ws://localhost:8443" });
+
+export function useNats() {
+  const [nats, setNats] = useState<NatsConnection | null>(null);
+
+  useEffect(() => {
+    if (!nats) {
+      natsClientPromise
+        .then((nc) => {
+          setNats(nc);
+        })
+        .catch((err) => console.error("connect failed", err));
+    }
+  }, []);
+
+  return nats;
+}
+
+type SuccessCallback = (msg: Msg) => Promise<void>;
+
+export function useNatsSubscription(subj: string, onMessage: SuccessCallback) {
+  const nc = useNats();
+  useEffect(() => {
+    if (!nc) return;
+    const sub = nc.subscribe(subj, {
+      callback: function (err: NatsError | null, msg: Msg) {
+        if (err) {
+          console.error(err);
+        } else {
+          onMessage(msg);
+        }
+      },
+    });
+    return () => {
+      sub.unsubscribe();
+    };
+  }, [nc]);
+}
