4e2b98f362dea60568fdb2d6743646a6032c0251
Robin Malfait <malfait.robin@gmail.com>
2024-05-03 00:16:05 +0200

    add `useIras` hook
    This hook will use the `useNatsSubscription` under the hood, but will
    also massage the incoming data to a data format you can work with.
    
    The data, that was only available in the callbacks, will now also be
    available in the hook (and the component that uses the hook), because we
    use `useReducer` to reduce the incoming data into a proper data
    structure.

--- hooks/useNats.tsx
+++ hooks/useNats.tsx
@@ -1,5 +1,5 @@
 import { connect, Msg, NatsConnection, NatsError } from "nats.ws";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer, useCallback } from "react";
 
 export const natsClientPromise = connect({ servers: "ws://localhost:8443" });
 
@@ -41,4 +41,76 @@ export function useNatsSubscription(
   }, [nc, onError, onMessage, subj]);
 }
 
+// TODO: Massage your data into a format that makes sense for your app.
+type StateDefinition = {
+  iras: Map<
+    string,
+    {
+      effect: "rainbow" | "strobe" | "fade" | "solid" | "off";
+      leds: Map<number, `#${string}`>;
+    }
+  >;
+  error: NatsError | null;
+};
+
+type Actions =
+  | { type: "NEW_MESSAGE"; payload: Msg }
+  | { type: "NATS_ERROR"; error: NatsError };
+
+function stateReducer(state: StateDefinition, action: Actions) {
+  switch (action.type) {
+    case "NEW_MESSAGE": {
+      const id = action.payload.subject;
+      const payload = action.payload.string();
+
+      const iras = new Map(state.iras);
+      const ira =
+        iras.get(id) ||
+        iras.set(id, { effect: "off", leds: new Map() }).get(id)!;
+
+      // Effects
+      if (
+        payload === "rainbow" ||
+        payload === "strobe" ||
+        payload === "fade" ||
+        payload === "solid" ||
+        payload === "off"
+      ) {
+        ira.effect = payload;
+      }
+
+      // Leds
+      else {
+        // I actually have no idea what `2#de7656 2#28cce1` means. 😅
+        for (let led of payload.split(" ")) {
+          const [index, color] = led.split("#");
+          ira.leds.set(Number(index), `#${color}`);
+        }
+      }
+
+      return { ...state, iras, error: null };
+    }
+
+    case "NATS_ERROR": {
+      return { ...state, error: action.error };
+    }
+
+    default:
+      return state;
+  }
+}
+
+export function useIras(subj: string) {
+  const [state, dispatch] = useReducer(stateReducer, {
+    iras: new Map(),
+    error: null,
+  } satisfies StateDefinition);
+
+  useNatsSubscription(
+    subj,
+    useCallback((msg) => dispatch({ type: "NEW_MESSAGE", payload: msg }), []),
+    useCallback((err) => dispatch({ type: "NATS_ERROR", error: err }), []),
+  );
+
+  return state;
 }
